722eb709d076af668cab8df6b3077223d77585a8
BenMClayton <ben.m.clayton.2003@gmail.com>
2024-02-27 19:02:59 +0000

    nothing (read desc)
    As I am new to c sharp and the .NET framework, I was just creating a quick and simple project to learn the syntax and how the language works.

---
+++ first/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double decimalNumber = 100E3;
+            int number = (int) decimalNumber;
+            
+            Console.WriteLine(number);
+        }
+    }
+}
